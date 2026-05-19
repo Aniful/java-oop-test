025b738f6eed98da8c2e1ce96a0c66ca4692f11f
Aniful <Aniful@list.ru>
2026-05-19 09:10:02 +0500

    Описаны get, set атрибута balance

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,2 +1,20 @@
 public class BankAccount {
+    private String accountNumber;
+    private double balance;
+    private String ownerName;
+
+    BankAccount(double balance, String ownerName) {
+        this.balance = balance;
+        this.ownerName = ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        if (balance >= 0) {
+            this.balance = balance;
+        }
+    }
 }
