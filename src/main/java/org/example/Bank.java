7084f5b9068ed9b3de449e877ae022be7d6b66e5
Aniful <Aniful@list.ru>
2026-05-27 11:56:13 +0500

    Добавлена обработка исключения для метода findCustomerByName

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Bank {
+    private ArrayList<Customer> arrayListCustomer = new ArrayList<Customer>();
+
+    public void addCustomer(Customer customer) {
+        arrayListCustomer.add(customer);
+        System.out.println("Добавление клиента выполнено успешно.");
+    }
+
+    public void findCustomerByName(String name) throws CustomerNotFoundException{
+        boolean isCustomer = false;
+
+        for(Customer customer : arrayListCustomer){
+            if (customer.getName() == name) {
+                isCustomer = true;
+                System.out.println("Клиент уже присутствует в базе данных.");
+            }
+        }
+
+        if (isCustomer == false) {
+            throw new CustomerNotFoundException("Клиент еще не добавлен в базу данных.");
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,20 +5,29 @@ package org.example;
 public class Main {
     static void main() throws Exception {
 
-        BankAccount firstUser = new BankAccount("009928283333", 900.0, "Filip G.");
+        BankAccount firstUser = new BankAccount("009928283333", 900.0, "Filip G");
         firstUser.getInfo();
         try {
 
-            firstUser.withraw(1000.0);
+            firstUser.withraw(100.0);
         } catch (InsufficientFundsException e) {
             System.out.println(e.getMessage());
         }
 
-//        User FilipCustomer = new Customer(firstUser.getOwnerName(), "Fil@li.su", firstUser);
+        Customer FilipCustomer = new Customer(firstUser.getOwnerName(), "Fil@li.su", firstUser);
 //        System.out.println(FilipCustomer.toString());
 //
-//        User FilipUser = new User(firstUser.getOwnerName(), "Fil@li.su");
+        User FilipUser = new User(firstUser.getOwnerName(), "Fil@li.su");
 //        System.out.println(FilipUser.toString());
+
+        Bank bank = new Bank();
+        bank.addCustomer(FilipCustomer);
+        try {
+            bank.findCustomerByName("Filip Gkjh");
+        } catch (CustomerNotFoundException e) {
+            e.getMessage();
+        }
+
 
 
     }
